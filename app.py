eefa5d54628122883f602e94820b24fee1fbb4b5
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-01-17 19:31:59 -0600

    set sqlalchemy base uri to heroku database url environment variable in app.py.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 bcrypt = Bcrypt()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///myreads'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///myreads')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'verysecret')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
