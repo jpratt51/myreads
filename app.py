0521bdcdb9e9ee53d5f332428dc356f0d60750b8
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-01-20 17:50:18 -0600

    Created register.py file and refactored registration routes from app.py to register.py.

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from book import *
 from bookshelf import *
 from favorite import *
 from account import *
+from register import *
 
 app = Flask(__name__)
 
@@ -126,126 +127,25 @@ def register_user():
 
     Upon successful registration add new user username to session for login."""
 
-    if "username" in session:
-        flash("Must be logged out", "info")
-        return redirect("/")
-
-    if "verified" in session:
-        verified = session["verified"]
-    else:
-        verified = False
-
-    if verified == True :
-        form = UserForm()
-        if form.validate_on_submit():
-            session.pop("verified")
-
-            u = User.query.filter_by(username=form.username.data).first()
-
-            if u :
-                flash(f"Username already in use", "info")
-                session["verified"] = True
-                return redirect('/register/register')
-
-            email = session["email"]
-
-            username = form.username.data
-            password = form.password.data
-            email = email
-            first_name = form.first_name.data
-            last_name = form.last_name.data
-            img_url = form.img_url.data
-            new_user = User.register(username, first_name, last_name, email,password, img_url)
-
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = new_user.username
-            session.pop("email")
-
-            flash(f'Welcome {new_user.username}! Account creation successful!', "success")
-            return redirect('/')
-        return render_template('/register/register.html', form=form)
-    return redirect("/register/send-code")
+    return register_account()
 
 @app.route("/register/send-code", methods=["GET","POST"])
 def send_code():
     """Generate form to verify user's email account. Sends six digit code to user's email for verification."""
 
-    if "username" in session:
-        flash("Must be logged out", "info")
-        return redirect('/')
-
-    form = SendCodeForm()
-    if form.validate_on_submit():
-
-        email = form.email.data
-        u = User.query.filter_by(email=email).first()
-
-        if u :
-            flash(f"Email already in use", "warning")
-            return redirect('/login')
-
-        session["email"] = email
-        
-        send_code_email()
-
-        flash(f"Verification code sent to {email}", "success")
-        return redirect('/register/verify-code')
-    return render_template('/register/send-code.html', form=form)
-
-def send_code_email():
-    """Send verification code to user email address"""
-
-    verify_code = randint(100000,999999)
-    session["code"] = verify_code
-    if "email" in session:
-        email = session["email"]
-    else:
-        flash("Oops, something went wrong", "danger")
-        return redirect("/register/send-code")
-
-    msg = Message('Myreads email verification', sender = 'myreadsverify@gmail.com', recipients = [email])
-    msg.body = f"Verification code: {verify_code}"
-    mail.send(msg)
+    return send_verification_code()
 
 @app.route('/register/resend-code')
 def resend_verification_code():
     """Resend verification code to user email for registration."""
 
-    if "username" in session:
-        flash("Must be logged out", "info")
-        return redirect('/')
-
-    try:
-        send_code_email()
-    except:
-        flash("Something went wrong. Please submit your email for a new verification code.", "primary")
-        return redirect('/register/send-code')
-
-    user_email = session["email"]
-
-    flash(f"Successfully re-sent verification code to {user_email}. Please enter the most recent verification code.", "success")
-    return redirect('/register/verify-code')
+    return resend_email_code()
 
 @app.route("/register/verify-code", methods=["GET","POST"])
 def code_verification():
     """Generate form to submit code for email verification for registration."""
-    form = VerifyEmailForm()
-
-    if "code" in session:
-        verify_code = session["code"]
-
-    if form.validate_on_submit():
-        if int(verify_code) == int(form.code.data):
-
-            session["verified"] = True
-            session.pop("code")
-
-            flash(f"Success! Email verified.", "success")
-            return redirect("/register/register")
-        else :
-            flash("Code is incorrect. Please enter verification code and resubmit.", "danger")
-    return render_template('/register/verify.html', form=form)
+    
+    return submit_code()
 
 # Password reset routes ********************************************************************************************************************************************************
 
---
+++ register.py
@@ -0,0 +1,154 @@
+"""Functions for registration routes for myreads app."""
+
+from flask import Flask, redirect, session, flash, render_template
+from models import db, connect_db, User
+from forms import UserForm, SendCodeForm, VerifyEmailForm
+from flask_mail import Mail, Message
+from random import randint
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRESQL_URL', 'postgresql:///myreads')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.config['MAIL_SERVER']='smtp-relay.sendinblue.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'myreadscode@gmail.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('SMTP_KEY')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
+connect_db(app)
+
+# registration routes
+# *************************************************************************
+# *************************************************************************
+
+def register_account():
+    """Generate form to register new user.
+
+    Upon successful registration add new user username to session for login."""
+
+    if "username" in session:
+        flash("Must be logged out", "info")
+        return redirect("/")
+
+    if "verified" in session:
+        verified = session["verified"]
+    else:
+        verified = False
+
+    if verified == True :
+        form = UserForm()
+        if form.validate_on_submit():
+            session.pop("verified")
+
+            u = User.query.filter_by(username=form.username.data).first()
+
+            if u :
+                flash(f"Username already in use", "info")
+                session["verified"] = True
+                return redirect('/register/register')
+
+            email = session["email"]
+
+            username = form.username.data
+            password = form.password.data
+            email = email
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            img_url = form.img_url.data
+            new_user = User.register(username, first_name, last_name, email,password, img_url)
+
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = new_user.username
+            session.pop("email")
+
+            flash(f'Welcome {new_user.username}! Account creation successful!', "success")
+            return redirect('/')
+        return render_template('/register/register.html', form=form)
+    return redirect("/register/send-code")
+
+def send_verification_code():
+    """Generate form to verify user's email account. Sends six digit code to user's email for verification."""
+
+    if "username" in session:
+        flash("Must be logged out", "info")
+        return redirect('/')
+
+    form = SendCodeForm()
+    if form.validate_on_submit():
+
+        email = form.email.data
+        u = User.query.filter_by(email=email).first()
+
+        if u :
+            flash(f"Email already in use", "warning")
+            return redirect('/login')
+
+        session["email"] = email
+        
+        send_code_email()
+
+        flash(f"Verification code sent to {email}", "success")
+        return redirect('/register/verify-code')
+    return render_template('/register/send-code.html', form=form)
+
+def send_code_email():
+    """Send verification code to user email address"""
+
+    verify_code = randint(100000,999999)
+    session["code"] = verify_code
+    if "email" in session:
+        email = session["email"]
+    else:
+        flash("Oops, something went wrong", "danger")
+        return redirect("/register/send-code")
+
+    msg = Message('Myreads email verification', sender = 'myreadsverify@gmail.com', recipients = [email])
+    msg.body = f"Verification code: {verify_code}"
+    mail.send(msg)
+
+def resend_email_code():
+    """Resend verification code to user email for registration."""
+
+    if "username" in session:
+        flash("Must be logged out", "info")
+        return redirect('/')
+
+    try:
+        send_code_email()
+    except:
+        flash("Something went wrong. Please submit your email for a new verification code.", "primary")
+        return redirect('/register/send-code')
+
+    user_email = session["email"]
+
+    flash(f"Successfully re-sent verification code to {user_email}. Please enter the most recent verification code.", "success")
+    return redirect('/register/verify-code')
+
+
+def submit_code():
+    """Generate form to submit code for email verification for registration."""
+    form = VerifyEmailForm()
+
+    if "code" in session:
+        verify_code = session["code"]
+
+    if form.validate_on_submit():
+        if int(verify_code) == int(form.code.data):
+
+            session["verified"] = True
+            session.pop("code")
+
+            flash(f"Success! Email verified.", "success")
+            return redirect("/register/register")
+        else :
+            flash("Code is incorrect. Please enter verification code and resubmit.", "danger")
+    return render_template('/register/verify.html', form=form)
